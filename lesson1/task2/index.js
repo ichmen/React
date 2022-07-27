9357ec13cd388fdfdb36eaeb8d046e0aa78a5bfb
ichmen <ichmen@rambler.ru>
2022-07-28 00:19:09 +0300

    l1t2

---
+++ lesson1/task2/index.js
@@ -0,0 +1,11 @@
+const rootElement = document.querySelector("#root");
+
+const greetingElement = React.createElement("div", { className: "greeting" }, [
+  React.createElement("h1", { className: "greeting__title" }, "Hello, world!"),
+  React.createElement(
+    "p",
+    { className: "greeting__text" },
+    "I'm learning React"
+  ),
+]);
+ReactDOM.render(greetingElement, rootElement);
