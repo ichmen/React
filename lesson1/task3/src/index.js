a807ca8065705f7062712b999511e8bfb5049740
ichmen <ichmen@rambler.ru>
2022-07-28 11:50:46 +0300

    quotes removed

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -3,8 +3,8 @@ const rootElement = document.querySelector("#root");
 
 const element = (
   <div class="greeting">
-    <h1 class="greeting__title">"Hello, world!"</h1>
-    <p class="greeting__text">"I am learning React"</p>
+    <h1 class="greeting__title">Hello, world!</h1>
+    <p class="greeting__text">I am learning React</p>
   </div>
 );
 
