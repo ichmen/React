965a0ae0d3b9a35a2ba876985d7139c23056daf3
ichmen <ichmen@rambler.ru>
2022-07-29 21:57:23 +0300

    l3t1

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Search from "./search.jsx";
+import "./index.scss";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Search name="Bob" />, rootElem);
