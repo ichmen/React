c893f52397e023a4231c3c95aebc2663c4d57c44
ichmen <ichmen@rambler.ru>
2022-07-30 19:03:24 +0300

    l3hw1

---
+++ lesson3/hw1/src/Greetings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react";
+import moment from "moment";
+
+export default function Ggreeting(props) {
+  return (
+    <div className="greeting">
+      `My name is ${props.firstName} ${props.secondName}. I'm{" "}
+      {moment(props.birthDate).diff(new Date(), "years")} years old`
+    </div>
+  );
+}
