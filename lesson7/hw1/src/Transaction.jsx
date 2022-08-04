8ad1090d5f40d58e9792d549bd687db6f1f78cf4
ichmen <ichmen@rambler.ru>
2022-08-04 17:32:44 +0300

    HH

--- lesson7/hw1/src/Transaction.jsx
+++ lesson7/hw1/src/Transaction.jsx
@@ -6,7 +6,7 @@ export default function Transactions({ from, to, amount, rate, time }) {
   return (
     <li className="transaction">
       <span className="transaction__date">{moment(time).format("DD MMM")}</span>
-      <span className="transaction__time">{moment(time).format("H:mm")}</span>
+      <span className="transaction__time">{moment(time).format("HH:mm")}</span>
       <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{numberFormatter.format(rate)}</span>
       <span className="transaction__amount">
