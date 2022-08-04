9bd9dd849b7ed6027af0cbb88031bb722ee0c78c
ichmen <ichmen@rambler.ru>
2022-08-04 17:11:28 +0300

    formatter created

--- lesson7/hw1/src/Transaction.jsx
+++ lesson7/hw1/src/Transaction.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import moment from "moment";
 
 export default function Transactions({ from, to, amount, rate, time }) {
+  const numberFormatter = new Intl.NumberFormat("en-GB");
+  //   console.log(numberFormatter.format(amount));
   return (
     <li className="transaction">
       <span className="transaction__date">{moment(time).format("DD MMM")}</span>
       <span className="transaction__time">{moment(time).format("H:mm")}</span>
       <span className="transaction__assets">{`${from} → ${to}`}</span>
-      <span className="transaction__rate">
-        {new Intl.NumberFormat("en-GB").format(rate)}
-      </span>
+      <span className="transaction__rate">{numberFormatter.format(rate)}</span>
       <span className="transaction__amount">
-        {new Intl.NumberFormat("en-GB").format(amount)}
+        {numberFormatter.format(amount)}
       </span>
     </li>
   );
