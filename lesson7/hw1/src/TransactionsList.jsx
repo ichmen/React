7a226f6f616b9e814b35cb2419c1c615dc95a826
ichmen <ichmen@rambler.ru>
2022-08-05 12:01:55 +0300

    ref

--- lesson7/hw1/src/TransactionsList.jsx
+++ lesson7/hw1/src/TransactionsList.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Transaction from "./Transaction";
 
 export default function TransactionsList(props) {
-  const numberFormatter = new Intl.NumberFormat("en-GB");
   const transactions = [...props.transactions];
-  transactions.forEach((element) => {
-    element.amount = numberFormatter.format(element.amount);
-    element.rate = numberFormatter.format(element.rate);
-  });
+  const numberFormatter = new Intl.NumberFormat("en-GB");
+  // transactions.forEach((element) => {
+  //   element.amount = numberFormatter.format(element.amount);
+  //   element.rate = numberFormatter.format(element.rate);
+  // });
   return (
     <ul className="transactions">
       {transactions.map((transaction) => (
-        <Transaction key={transaction.id} {...transaction} />
+        <Transaction
+          key={transaction.id}
+          {...transaction}
+          amount={numberFormatter.format(transaction.amount)}
+          rate={numberFormatter.format(transaction.rate)}
+        />
       ))}
     </ul>
   );
