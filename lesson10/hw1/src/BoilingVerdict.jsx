1b76e7eb65f421da263ac03151b252e57e60e8ad
ichmen <ichmen@rambler.ru>
2022-08-09 20:24:07 +0300

    . added

--- lesson10/hw1/src/BoilingVerdict.jsx
+++ lesson10/hw1/src/BoilingVerdict.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function BoilingVerdict(props) {
   return props.celsius >= 100 ? (
-    <p>The water would boil</p>
+    <p>The water would boil.</p>
   ) : (
-    <p>The water would not boil</p>
+    <p>The water would not boil.</p>
   );
 }
