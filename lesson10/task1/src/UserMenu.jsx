80c672f065e335178e13d48bb56f51338afdcc37
ichmen <ichmen@rambler.ru>
2022-08-09 12:20:45 +0300

    new

--- lesson10/task1/src/UserMenu.jsx
+++ lesson10/task1/src/UserMenu.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-export default function UserMenu({ userData: { name, avatar_url } }) {
+export default function UserMenu({ userData }) {
+  if (!userData) {
+    return null;
+  }
+
   return (
     <div className="menu">
-      <span className="menu__greeting">{`Hello, ${name}`}</span>
-      <img alt="User Avatar" src={avatar_url} className="menu__avatar" />
+      <span className="menu__greeting">{`Hello, ${userData.name}`}</span>
+      <img
+        alt="User Avatar"
+        src={userData.avatar_url}
+        className="menu__avatar"
+      />
     </div>
   );
 }
