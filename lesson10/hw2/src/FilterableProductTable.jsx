9adddacbe7ef905101b198d43f43f233343c16f4
ichmen <ichmen@rambler.ru>
2022-08-10 15:19:26 +0300

    new

--- lesson10/hw2/src/FilterableProductTable.jsx
+++ lesson10/hw2/src/FilterableProductTable.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
-
+import products from "./products.js";
 export default class FilterableProductTable extends React.Component {
   constructor(props) {
     super(props);
@@ -27,7 +27,7 @@ export default class FilterableProductTable extends React.Component {
           onClick={this.checkboxChangeHandler}
         />
         <ProductTable
-          products={this.props.products}
+          products={products}
           filterText={this.state.filterText}
           inStockOnly={this.state.inStockOnly}
         />
