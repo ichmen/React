b2df0f4daddac9561c3255d558c172390abc0208
ichmen <ichmen@rambler.ru>
2022-08-11 16:06:09 +0300

    moved to component

--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
-export default function Expand({ children, title, isVisible, clickHandler }) {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={() => clickHandler()}>
-          <i
-            className={
-              isVisible ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down"
-            }
-          ></i>
-        </button>
+export default class Expand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isVisible: false };
+  }
+  clickHandler = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+  };
+
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button
+            className="expand__toggle-btn"
+            onClick={() => this.clickHandler()}
+          >
+            <i
+              className={
+                this.state.isVisible
+                  ? "fa-solid fa-chevron-up"
+                  : "fa-solid fa-chevron-down"
+              }
+            ></i>
+          </button>
+        </div>
+        {this.state.isVisible && (
+          <div className="expand__content">
+            <p>{this.props.children}</p>
+          </div>
+        )}
       </div>
-      <div className="expand__content">
-        <p>{isVisible && children}</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
