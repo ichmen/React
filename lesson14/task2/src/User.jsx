b2c492124015f78c52b2c2a98e88b8dfbb4850b5
ichmen <ichmen@rambler.ru>
2022-08-14 20:05:06 +0300

    l14t2

--- lesson14/task2/src/User.jsx
+++ lesson14/task2/src/User.jsx
@@ -3,25 +3,24 @@ const baseUrl = "https://api.github.com/users/";
 
 export default function User({ match }) {
   const [userData, setState] = useState(null);
-
+  // console.log(match.params.userId);
   useEffect(() => {
-    fetch(baseUrl + match.params.userName)
+    fetch(baseUrl + match.params.userId)
       .then((response) => response.json())
       .then((userData) => setState(userData));
-  }, [match.params.userData]);
+  }, [match.params.userId]);
   console.log(userData);
-  return;
-  // (
-  //   <div className="user">
-  //     <img
-  //       alt="User Avatar"
-  //       src={this.state.avatar_url}
-  //       className="user__avatar"
-  //     />
-  //     <div className="user__info">
-  //       <span className="user__name">{this.state.name}</span>
-  //       <span className="user__location">{this.state.location}</span>
-  //     </div>
-  //   </div>
-  // );
+  if (!userData) {
+    return;
+  }
+  const { avatar_url, name, location } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
 }
